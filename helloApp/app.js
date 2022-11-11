411c3333443730f6a5371eb2bc9ee8fb7c3bb197
Just_Alexey <60109266+AlexeyBobkovv@users.noreply.github.com>
2022-11-11 21:04:13 +0300

    feat: modules_1

--- helloApp/app.js
+++ helloApp/app.js
@@ -1,9 +1,8 @@
-const http = require("http");
+const os = require("os");
+const greeting = require("./greeting");
 
-http
-  .createServer(function (request, response) {
-    response.end("Hello NodeJS!");
-  })
-  .listen(3000, "127.0.0.1", function () {
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-  });
+// получим имя текущего пользователя
+let userName = os.userInfo().username;
+
+console.log(`Дата запроса: ${greeting.date}`);
+console.log(greeting.getMessage(userName));
